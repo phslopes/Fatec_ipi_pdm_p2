1639d8cfc4c956ff3d5cc57f3d29421dadfb4f1d
Pedro <pedroppzs012gmail.com>
2024-11-22 04:29:25 -0300

    feat(p2-pdm) exibe lista com 5 imagens

--- pdmgato/App.tsx
+++ pdmgato/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FlatList, Image, StyleSheet, View } from 'react-native';
 import ListaImagens from './component/ListaImagens';
+
 export default function App() {
   const [imagens, setImagens] = useState<string[]>([]); 
 
@@ -10,8 +11,9 @@ export default function App() {
       <FlatList
         data={imagens}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => <Image source={{ uri: item }} style={styles.image} />}
-        style={styles.list}
+        renderItem={({ item }) => 
+        <Image source={{ uri: item }} style={styles.imagem} />}
+        style={styles.lista}
       />
     </View>
   );
@@ -24,18 +26,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  list:   {
+  lista:   {
     width:'100%',
     marginTop: 80,
     borderRadius: 4,
     padding: 4,
     alignContent:'center',
-    
   },
-  image: {
-    width: 300,
-    height: 300,
+  imagem: {
+    width: 300, 
+    height: 200,
     marginBottom: 10,
     borderRadius: 10,
+    alignSelf: 'center', 
   },
 });
--- pdmgato/component/ListaImagens.tsx
+++ pdmgato/component/ListaImagens.tsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
-import { Image, StyleSheet, View, Pressable, Text } from 'react-native';
+import {StyleSheet, View, Pressable, Text } from 'react-native';
 import theCatApiClient from '../utils/theCatApiClient';
 import React from 'react';
+
 export default function ListaImagens({ setImagens }: { setImagens: (imagens: string[]) => void }) {
   const carregarImagens = async () => {
-      const response = await theCatApiClient.get('search', { params: { limit: 10 } }); 
-			const novasImagens = response.data.map((item: { url: string }) => item.url); 
+      const response = await theCatApiClient.get('search', { params: { limit: 5 } }); 
+			const novasImagens = response.data.slice(0, 5).map((item: any) => item.url);
       setImagens(novasImagens);
   };
 
   return (
     <View style={styles.container}>
       <Pressable style={styles.button} onPress={carregarImagens}>
-        <Text style={styles.buttonText}>Gerar Imagens</Text>
+        <Text style={styles.buttonText}>Gerar imagens</Text>
       </Pressable>
     </View>
   );
@@ -23,9 +23,9 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   button:   {
-    backgroundColor: '#0096F3',
+    backgroundColor: '#4e7df2',
     padding: 12,
-    borderRadius: 4,
+    borderRadius: 24,
     margin: 12,
   },
   buttonText:   {
